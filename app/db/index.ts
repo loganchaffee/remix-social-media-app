a7bbf33acb6cfa62fac348769f3974c80f577191
Logan Chaffee <79607299+loganchaffee@users.noreply.github.com>
2024-07-21 18:32:51 -0700

    Use DB connection pool

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,11 +1,14 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2";
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   database: process.env.MYSQL_DATABASE,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export const db = drizzle(connection);
